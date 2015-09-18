5705beb2ce4003df2e7974588d8ec5b909397631
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2015-09-17 23:28:57 -0500

    Do checks again.

--- check.sh
+++ check.sh
@@ -2,10 +2,10 @@ for f in $(git ls-files | grep '.md$'); do
   aspell -c $f
 done
 
-stack exec 'site check -i'
-
-# for f in $(find . -name "*.md" -not -path "./_site/*"); do
+# for f in $(git ls-files | grep '.md$'); do
 #     diction -bs $f | less
 # done
 
 # style?
+
+stack exec site check
